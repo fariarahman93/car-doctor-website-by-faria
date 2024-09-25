010e9b14895eb33b49992119d0dff91f08497a78
fariarahamn93 <fariarahman.tora@gmail.com>
2024-09-25 12:26:18 -0600

    Initial commit of Next.js project

---
+++ src/app/signup/api/route.js
@@ -0,0 +1,18 @@
+import { connectDB } from "@/lib/connectDB";
+
+export const POST = async(request) =>{
+    const newUser = await request.json();
+    try{
+   const db = await connectDB();
+   const userCollection =db.collection('users')
+   const exist =await userCollection.findOne({email: newUser.email})
+   if(exist) {
+    return Response.json({message:"user exists"},{status:304})
+   }
+   const resp =await userCollection.insertOne(newUser)
+   return Response.json({message:"user created"},{status:200})
+    }
+    catch(error) {
+        return Response.json({message:"something went wrong"},{status:500})
+    }
+}
